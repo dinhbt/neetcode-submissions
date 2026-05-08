f16c050d262a7c2d716d499654741ce00bf0dd90
dinhbt <61791079+dinhbt@users.noreply.github.com>
2026-05-08 14:09:03 +0700

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.js
@@ -0,0 +1,42 @@
+class MinStack {
+    stack =[];
+
+    constructor() {}
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        if (this.stack.length === 0) {
+            this.stack.push({ val: val, min: val });
+        } else {
+            const last = this.stack[this.stack.length - 1];
+            const min = Math.min(last.min, val);
+            this.stack.push({ val: val, min });
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        const size = this.stack.length - 1;
+        return  this.stack[size].val;
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {    
+        const size = this.stack.length - 1;
+        return  this.stack[size].min;
+    }
+}
